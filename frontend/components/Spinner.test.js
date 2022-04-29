ee6f4b942bed9d7d6008da4b8709b9b305d34134
unknown <dshipuk@yahoo.com>
2022-04-29 10:52:32 -0700

    Added Tests to check that spinner is in document when on={true} and spinner is not in the document when on={false}

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,6 +1,31 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
+import Spinner from "./Spinner"
+import React from "react"
+import { render } from "@testing-library/react"
+
 test('sanity', () => {
   expect(true).toBe(true)
 })
 
+it('renders Spinner without crashing', () => {
+  render(<Spinner />)
+})
+
+it("When Spinner On is True. Spinner is truthy", () => {
+  render(<Spinner on={true} />)
+
+  const spinner = document.querySelector("#spinner")
+  
+  expect(spinner).toBeTruthy()
+
+})
+
+it("When Spinner On is False, Spinner is Falsy", () => {
+  render(<Spinner on={false} />)
+
+  const spinner = document.querySelector("#spinner")
+  
+  expect(spinner).toBeFalsy()
+
+})
